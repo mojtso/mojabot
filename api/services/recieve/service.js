b79f768021eb27e573ba75b95430ce895f4024c4
Mojalefa <cmtsoaeli@gmail.com>
2018-01-09 16:01:58 +0200

    data in 0

--- api/services/recieve/service.js
+++ api/services/recieve/service.js
@@ -22,9 +22,44 @@ class RecieveService extends BaseService {
             switch (quickReplyPayload.payload) {
                 case '0':
                     //select movies genre
-                    var response = movies();
-                    console.log("--- Movies IN QUICK REPLY ---");
-                    console.log(response);
+                    // var response = movies();
+                    // console.log("--- Movies IN QUICK REPLY ---");
+                    // console.log(response);
+                    let data = {
+                        "attachment":{
+                            "type":"template",
+                            "payload":{
+                              "template_type":"generic",
+                              "elements":[
+                                 {
+                                  "title":"Welcome to Peter'\''s Hats",
+                                  "image_url":"https://petersfancybrownhats.com/company_image.png",
+                                  "subtitle":"We'\''ve got the right hat for everyone.",
+                                  "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                  },
+                                  "buttons":[
+                                    {
+                                      "type":"web_url",
+                                      "url":"https://petersfancybrownhats.com",
+                                      "title":"View Website"
+                                    },
+                                    {
+                                      "type":"postback",
+                                      "title":"Start Chatting",
+                                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                    }              
+                                  ]      
+                                }
+                              ]
+                            }
+                        }
+                    };
+                    console.log(data);
                     break;
                 case '1':
                     //select news category
@@ -83,40 +118,7 @@ class RecieveService extends BaseService {
     movies() {
         // Action. Adventure. Animation. Biography. Comedy. Crime.
         // Horror. Music. Musical. Mystery. Romance. Sci-Fi.
-        let data = {
-            "attachment":{
-                "type":"template",
-                "payload":{
-                  "template_type":"generic",
-                  "elements":[
-                     {
-                      "title":"Welcome to Peter'\''s Hats",
-                      "image_url":"https://petersfancybrownhats.com/company_image.png",
-                      "subtitle":"We'\''ve got the right hat for everyone.",
-                      "default_action": {
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                      },
-                      "buttons":[
-                        {
-                          "type":"web_url",
-                          "url":"https://petersfancybrownhats.com",
-                          "title":"View Website"
-                        },
-                        {
-                          "type":"postback",
-                          "title":"Start Chatting",
-                          "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                        }              
-                      ]      
-                    }
-                  ]
-                }
-            }
-        };
+        
         
         return data;
     }

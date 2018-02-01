5086a909ee4d5cb36fe7ca875f23332d61a8a0e8
Mojalefa <cmtsoaeli@gmail.com>
2018-02-01 17:11:38 +0200

    configing

--- config/config.js
+++ config/config.js
@@ -11,24 +11,18 @@
 
 module.exports = {
     development: {
-        username: 'root',
-        password: null,
-        database: 'database_development',
+        username: 'postgres',
+        password: 'MyNewPass',
+        database: 'subscribe_db',
         host: '127.0.0.1',
-        dialect: 'mysql'
+        dialect: 'postgres'
     },
-    test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        dialect: 'mysql'
-    },
+    
     production: {
         username: process.env.PROD_DB_USERNAME,
         password: process.env.PROD_DB_PASSWORD,
         database: process.env.PROD_DB_NAME,
         host: process.env.PROD_DB_HOSTNAME,
-        dialect: 'mysql'
+        dialect: 'postgres'
     }
 };

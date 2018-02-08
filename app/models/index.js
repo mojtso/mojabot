eb1317c07eb04b3f58235914fb3bbaf97b4dd556
Mojalefa <cmtsoaeli@gmail.com>
2018-02-08 12:32:17 +0200

    log env

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV || 'development';
 // var config = require(__dirname + '/../config/config.js')[env];
 import config from '../config/config';
 
-
+console.log(env);
 if(config.use_env_variable) {
     console.log("...........running prod");
     var sequelize = new Sequelize(process.env[config.use_env_variable], config);

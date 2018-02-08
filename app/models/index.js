3b9ed5f41224b6f4019b416286b3d3f414f8524a
Mojalefa <cmtsoaeli@gmail.com>
2018-02-08 13:44:08 +0200

    clean /app/models/index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,13 @@
 import Sequelize from 'sequelize';
 
 var env = process.env.NODE_ENV || 'development';
-// var config = require(__dirname + '/../config/config.js')[env];
-import config from '../config/config.js';
 
-console.log(config.use_env_variable);
-console.log(process.env.NODE_ENV);
 
 if(process.env.NODE_ENV === 'production') {
-    console.log("...........running prod");
+    console.log("...........running ", env);
     var sequelize = new Sequelize(process.env.DATABASE_URL, config);
 }else {
-    console.log("...........running dev");
+    console.log("...........running ", env);
     var sequelize = new Sequelize('subscribers_db', process.env.NODE_POSTGRES_USER, process.env.NODE_POSTGRES_PASSWORD, {
         dialect: 'postgres',
         define: {
